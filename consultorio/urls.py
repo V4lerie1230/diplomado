64a73033e6504ec18cda9ae0e959804df2977805
V4lerie1230 <106413029+V4lerie1230@users.noreply.github.com>
2022-05-28 18:57:37 -0500

    promesas6

--- consultorio/urls.py
+++ consultorio/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
-from jugadores import views
+from paciente import views
 
 app_name = 'consultorio'
 urlpatterns = [
