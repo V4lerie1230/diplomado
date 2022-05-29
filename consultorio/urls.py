583cea235b10270f663a7e8f373548fdf0c18285
V4lerie1230 <106413029+V4lerie1230@users.noreply.github.com>
2022-05-28 19:06:06 -0500

    promesas4

--- consultorio/urls.py
+++ consultorio/urls.py
@@ -1,20 +1,20 @@
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
-from paciente import views
+from jugadores import views
 
 app_name = 'consultorio'
 urlpatterns = [
     path('', TemplateView.as_view(template_name="home.html"), name="home"),
-    path('pacientes', TemplateView.as_view(template_name="jugadores.html"), name="jugadores"),
+    path('jugadores', TemplateView.as_view(template_name="jugadores.html"), name="jugadores"),
     path('medicos', TemplateView.as_view(template_name="medicos.html"), name="medicos"),
     path('especialidades', TemplateView.as_view(template_name="especialidades.html"), name="especialidades"),
     path('citas', views.numero_de_citas, name="numero_citas"),
     path('lista_doctores', views.get_doctores_list, name="lista-doctores"),
-    path('lista_pacientes', views.PacienteListView.as_view(), name="lista-pacientes"),
-    path('lista_pacientes/crear', views.PacienteCreateView.as_view(), name="crear-jugadores"),
+    path('lista_jugadores', views.PacienteListView.as_view(), name="lista-pacientes"),
+    path('lista_jugadores/crear', views.PacienteCreateView.as_view(), name="crear-jugadores"),
     path('jugadores/<int:pk>/', views.PacienteDetailView.as_view(), name='detalle-jugadores'),
-    path('paciente/<int:pk>/eliminar/', views.PacienteDeleteView.as_view(), name='eliminar-jugadores'),
-    path('paciente/<int:pk>/actualizar/', views.PacienteDeleteView.as_view(), name='actualizar-jugadores'),
+    path('jugadores/<int:pk>/eliminar/', views.PacienteDeleteView.as_view(), name='eliminar-jugadores'),
+    path('jugadores/<int:pk>/actualizar/', views.PacienteDeleteView.as_view(), name='actualizar-jugadores'),
 
 ]
